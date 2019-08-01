174a39bebe729da5b98d8ce1fd792ab740ce5ca3
shin donggyu <donggyu9410@gmail.com>
2019-08-01 17:51:56 +0900

    Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from './actions'
 
 function App() {
+  const count = useSelector(state => state.counterReducer);
+  const isLogged = useSelector(state => state.isLoggedReducer);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>
-        Hello
+        count: {count}
       </h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button>-</button>
+      <h3>
+        {isLogged ? 'logged in' : 'not logged in'}
+      </h3>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,51 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
+import allReducer from './reducers'
+import { Provider } from 'react-redux'
+
+const store = createStore(allReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
+ReactDOM.render(
+    <Provider store={store}>
 
-// STORE -> GLOBALIZED STATE
-// stores all states, datas of application
-
-
-// ACTION -> Kind of describes what you want to do
-// Simple function that returns object
-const increment = () => {
-    return {
-        type: 'INCREMENT'
-    }
-}
-
-const decrement = () => {
-    return {
-        type: 'DECREMENT'
-    }
-}
-
-// REDUCER -> Describes how your actions transforms the state into the next state
-
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1
-
-        case 'DECREMENT':
-            return state - 1
-        default:
-            break;
-    }
-}
-
-let store = createStore(counter);
-
-// Display it in the console.
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCH -> Actually execute that action
-store.dispatch(increment())
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
