b300380b5ec23ec5875617be2baee018a342a8c8
shin donggyu <donggyu9410@gmail.com>
2019-08-01 17:23:54 +0900

    Introduce what redux is

--- src/index.js
+++ src/index.js
@@ -3,6 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+
+
+
+// STORE -> GLOBALIZED STATE
+// stores all states, datas of application
+
+
+// ACTION -> Kind of describes what you want to do
+// Simple function that returns object
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+// REDUCER -> Describes how your actions transforms the state into the next state
+
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+
+        case 'DECREMENT':
+            return state - 1
+        default:
+            break;
+    }
+}
+
+let store = createStore(counter);
+
+// Display it in the console.
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH -> Actually execute that action
+store.dispatch(increment())
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
